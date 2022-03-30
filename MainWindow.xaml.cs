432a4ebc1543bb8584678d1c295c9d63ace35467
Junior Oliveira <junioroliveira@hotmail.com>
2022-03-30 16:26:17 -0300

    Update Beta

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,11 @@ using System.Windows.Shapes;
 
 namespace DownloadArquivos
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
     public MainWindow()
         {
             InitializeComponent();
@@ -138,4 +143,6 @@ namespace DownloadArquivos
                 Start(tWeb.Text);
             }
         }
+
+    }
 }
